c1a056a7dea745f2ab2e970a9e2c9d12731be4e4
tom <non_pooh2007@yahoo.co.jp>
2012-11-02 08:19:55 +0900

    update

--- web.rb
+++ web.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
 get '/' do
-  "<p>what your name </p><form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
+  "<p>what your name! </p>"
+   +"<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
 end
