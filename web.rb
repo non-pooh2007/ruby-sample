c5e8aff0cdcda12ce3c1e6cd56a304639522f772
tom <non_pooh2007@yahoo.co.jp>
2012-11-03 11:02:28 +0900

    add & update

--- web.rb
+++ web.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
+reuuire 'haml'
 
 get '/' do
+  haml :sample
   "<p>what your name! </p>"
-  "<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
+  #  "<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
 end
