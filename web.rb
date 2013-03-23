a4b85a8921f0a5568c0194a9d2d8761473c1ac00
tom <non_pooh2007@yahoo.co.jp>
2013-03-23 18:28:23 +0900

    change message

--- web.rb
+++ web.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
-  "<p>what your name! </p>"
+  "<p>what your name!? </p>"
   "<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
 end
 
