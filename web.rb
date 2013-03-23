a68c7d63e56ff72380ce61f4420434e243f8a754
tom <non_pooh2007@yahoo.co.jp>
2013-03-23 18:38:29 +0900

    change string process

--- web.rb
+++ web.rb
@@ -2,8 +2,7 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
-  "<p>what your name!? </p>"
-  "<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
+  "<p>what your name!? </p>"+"<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
 end
 
 get '/haml' do
