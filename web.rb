b4940675ba196629eaaf966733c6fba0730c9a8f
tom <non_pooh2007@yahoo.co.jp>
2013-06-04 21:41:57 +0900

    change web.rb: add google.haml

--- web.rb
+++ web.rb
@@ -2,9 +2,13 @@ require 'sinatra'
 require 'haml'
 
 get '/' do
-  "<p>what your name!? </p>"+"<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
+  "<p>What your name? </p>"+"<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
 end
 
 get '/haml' do
   haml :sample
+end
+
+get '/google' do
+  haml :google
 end
