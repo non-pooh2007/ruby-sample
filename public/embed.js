5f94db167f45728a976843ad2d3beeb732104969
tom <non_pooh2007@yahoo.co.jp>
2013-06-16 06:15:03 +0900

    ok

--- public/embed.js
+++ public/embed.js
@@ -5,7 +5,7 @@
 
     var pusher = new Pusher('92577fdc22964f716bc6');
     var channel = pusher.subscribe('test_channel');
-    channel.bind('tom', function(data) {
+    channel.bind(bind_event, function(data) {
       alert(data.id + data.message);
     });
 
