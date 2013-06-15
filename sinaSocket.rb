7e5b7f1d9d14d644aa896f67769087bc958dbd2c
tom <non_pooh2007@yahoo.co.jp>
2013-06-16 04:58:27 +0900

    comment out json

--- sinaSocket.rb
+++ sinaSocket.rb
@@ -10,20 +10,24 @@ set :sockets, []
 $users = []
 
 get '/login' do
-  "<p>What your name? </p>"+"<form action='/hello' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
+  "<p>What your name? </p>"+"<form action='/login' method='POST'><input type='text' name='name'><input type='submit' value='send'></form>"
 end
 
-post '/hello' do
+post '/login' do
   warn( params )
   $users.push( params['name'] )
   warn( $users )
+  [ 200, { 'Content-Type' => 'text/html' },
+  ['<html><body>' + params['name'] + '</body></html>'] ]
+
 #  data = JSON.parse request.body.read
 #  warn( "params" + params )
 #  warn( "body" + data )
 end
 
 get '/pusher' do
-  Pusher['test_channel'].trigger('my_event', {:id => 1234, :message => 'hello world'})
+  # Pusher['test_channel'].trigger('my_event', {:id => 1234, :message => 'hello world'})
+  Pusher['test_channel'].trigger( $users, {:id => 1234, :message => 'hello world'})
 end
 
 get '/' do
