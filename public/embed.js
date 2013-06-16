bdd7431b87d45720956239aeadff1494ef11caa1
tom <non_pooh2007@yahoo.co.jp>
2013-06-16 18:57:18 +0900

    add search_pos.html

--- public/embed.js
+++ public/embed.js
@@ -1,3 +1,4 @@
+    // not use
     // Enable pusher logging - don't include this in production
     Pusher.log = function(message) {
       if (window.console && window.console.log) window.console.log(message);
